7430f0dc119b9661439cf858809c5e30f151c078
andyxob <a.xlopot@g,ail.com>
2022-05-30 11:35:33 +0300

    crud for users

--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\MainController::class, 'index'])->middleware(['auth'])->name('dashboard');
 Route::get('/search', [\App\Http\Controllers\SearchController::class, 'getResults'])->name('search.results');
 
-Route::get('/user/{username}', [\App\Http\Controllers\ProfileController::class, 'getProfile'])->name('profile.index');
-Route::get('profile/edit', [\App\Http\Controllers\ProfileController::class, 'getEdit'])->middleware('auth')->name('profile.edit');
-Route::post('profile/edit', [\App\Http\Controllers\ProfileController::class, 'postEdit'])->middleware('auth')->name('profile.edit');
+
+Route::group(['middleware'=>'auth'], function (){
+
+    Route::get('/user/{username}', [\App\Http\Controllers\ProfileController::class, 'getProfile'])->name('profile.index');
+    Route::get('profile/edit', [\App\Http\Controllers\ProfileController::class, 'getEdit'])->name('profile.edit');
+    Route::post('profile/edit', [\App\Http\Controllers\ProfileController::class, 'postEdit'])->name('profile.edit');
+
+    Route::group( ['prefix' => 'friends'], function () {
+        Route::get('/', [\App\Http\Controllers\FriendController::class, 'getIndex'])->name('friends.index');
+        Route::get('/add/{name}', [\App\Http\Controllers\FriendController::class, 'getAdd'])->name('friends.add');
+        Route::get('/accept/{name}', [\App\Http\Controllers\FriendController::class, 'getAccept'])->name('friends.accept');
+        Route::get('/delete/{name}', [\App\Http\Controllers\FriendController::class, 'postDelete'])->name('friends.delete');
 
 
-Route::group(['middleware' => 'auth', 'prefix' => 'friends'], function () {
-    Route::get('/', [\App\Http\Controllers\FriendController::class, 'getIndex'])->name('friends.index');
-    Route::get('/add/{name}', [\App\Http\Controllers\FriendController::class, 'getAdd'])->name('friends.add');
-    Route::get('/accept/{name}', [\App\Http\Controllers\FriendController::class, 'getAccept'])->name('friends.accept');
-    Route::get('/delete/{name}', [\App\Http\Controllers\FriendController::class, 'postDelete'])->name('friends.delete');
-
+        Route::group(['prefix' => 'status'], function () {
+            Route::post('/', [\App\Http\Controllers\StatusController::class, 'postStatus'])->middleware('auth')->name('status.post');
+            Route::post('/{StatusId}/reply', [\App\Http\Controllers\StatusController::class, 'postReply'])->middleware('auth')->name('status.reply');
+            Route::get('/{StatusId}/like', [\App\Http\Controllers\StatusController::class, 'getLike'])->middleware('auth')->name('status.like');
+        });
+    });
 });
+
+
 Route::group(['prefix' => 'admin', 'middleware' => 'is_admin'], function () {
     Route::get('/', [\App\Http\Controllers\MainController::class, 'admin'])->name('admin.index');
     Route::resource('statuses', \App\Http\Controllers\Admin\StatusController::class);
@@ -35,11 +46,6 @@ Route::group(['prefix' => 'admin', 'middleware' => 'is_admin'], function () {
 
 });
 
-Route::group(['prefix' => 'status', 'middleware' => 'auth'], function () {
-    Route::post('/', [\App\Http\Controllers\StatusController::class, 'postStatus'])->middleware('auth')->name('status.post');
-    Route::post('/{StatusId}/reply', [\App\Http\Controllers\StatusController::class, 'postReply'])->middleware('auth')->name('status.reply');
-    Route::get('/{StatusId}/like', [\App\Http\Controllers\StatusController::class, 'getLike'])->middleware('auth')->name('status.like');
 
-});
 
 require __DIR__ . '/auth.php';
